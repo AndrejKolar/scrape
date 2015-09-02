dd3f17d44e8e1dfcc14fc7961f6a23a5816e0fe8
Andrej Kolar <andrej.kolar@me.com>
2015-09-02 12:06:08 +0200

    Ordered code

--- scrape.rb
+++ scrape.rb
@@ -8,63 +8,52 @@ require 'nokogiri'
 require 'open-uri'
 
 require 'mechanize'
+require 'uri'
 
 START_LINK = "http://alpha.wallhaven.cc/random"
 
-def downaload_images_from_array(images)
-  index = 0
-  images.each do |image|
-  download_image(image, index)
-  index.next
-  end
+def download_images_from_array(images)
+    images.each do |image|
+        download_image(image)
+    end
 end
 
-def download_image(image_link, index)
-  agent = Mechanize.new
-  agent.get(image_link).save "images/pic" + index.to_s + ".jpg"
+def download_image(image_link)
+    name = URI(image_link).path.split('/').last
+    agent = Mechanize.new
+    agent.get(image_link).save "images/" + name
 end
 
 def create_full_link(image_link)
-  image_link.slice!(0..1)
-  image_link = "http://" + image_link
-
-  p image_link
-
-  image_link
+    image_link.slice!(0..1)
+    image_link = "http://" + image_link
+    image_link
 end
 
 def clear_images_folder
-  Dir.foreach("images/") do |file|
-          next if file.start_with?('.')
-          next unless file.end_with?(".jpg")
-
-          # Delete
-          filePath = File.join("images/", file)
-          File.delete(filePath)
-   end
+    Dir.foreach("images/") do |file|
+        next if file.start_with?('.')
+        filePath = File.join("images/", file)
+        File.delete(filePath)
+    end
 end
 
 def get_link_array
+    links = Array.new
 
-  links = Array.new
+    main_page = Nokogiri::HTML(open(START_LINK))
+    page_links = main_page.css("a").select{|link| link['class'] == "preview"}
 
-  main_page = Nokogiri::HTML(open(START_LINK))
-  page_links = main_page.css("a").select{|link| link['class'] == "preview"}
-
-  page_links.each do |thumb_link|
-    href_page_link = thumb_link["href"]
-    image_page = Nokogiri::HTML(open(href_page_link))
-    image_link = image_page.css("img#wallpaper").attribute("src").value
-
-    image_link = create_full_link(image_link)
-    links.push(image_link)
-
-  end
+    page_links.each do |thumb_link|
+        href_page_link = thumb_link["href"]
+        image_page = Nokogiri::HTML(open(href_page_link))
+        image_link = image_page.css("img#wallpaper").attribute("src").value
+        image_link = create_full_link(image_link)
+        links.push(image_link)
+    end
+    links
 end
 
-#main
-
+# Main
 clear_images_folder
-links = get_link_array
-downaload_images_from_array(links)
-
+download_images_from_array(get_link_array)
