a1d085e71e6234bbfc05d6d97d05db8645ad21d9
Andrej Kolar <andrej.kolar@me.com>
2015-09-02 15:43:34 +0200

    Added progress printout

--- scrape.rb
+++ scrape.rb
@@ -11,18 +11,14 @@ require 'uri'
 
 START_LINK = "http://alpha.wallhaven.cc/search?categories=111&purity=100&resolutions=2560x1440&sorting=random&order=desc"
 
-def desktops
-  system_events.desktops
-end
-
-def set_desktop
-  desktops.picture.set(wallpaper_path)
-end
-
 def download_images_from_array(images)
+    index = 1
     images.each do |image|
+        print "\rDownloading images #{index} / #{images.count}"
         download_image(image)
+        index = index.next
     end
+    print "\rScraping image links #{images.count} / #{images.count} ...done\n"
 end
 
 def download_image(image_link)
@@ -38,11 +34,13 @@ def create_full_link(image_link)
 end
 
 def clear_images_folder
-    Dir.foreach("images/") do |file|
+    print "Deleting old images"
+    Dir.foreach("./images/") do |file|
         next if file.start_with?('.')
-        filePath = File.join("images/", file)
+        filePath = File.join("./images/", file)
         File.delete(filePath)
     end
+    print " ...done\n"
 end
 
 def get_link_array
@@ -51,13 +49,17 @@ def get_link_array
     main_page = Nokogiri::HTML(open(START_LINK))
     page_links = main_page.css("a").select{|link| link['class'] == "preview"}
 
+    index = 1
     page_links.each do |thumb_link|
         href_page_link = thumb_link["href"]
         image_page = Nokogiri::HTML(open(href_page_link))
         image_link = image_page.css("img#wallpaper").attribute("src").value
         image_link = create_full_link(image_link)
         links.push(image_link)
+        print "\rScraping image links #{index} / #{page_links.count}"
+        index = index.next
     end
+    print "\rScraping image links #{page_links.count} / #{page_links.count} ...done\n"
     links
 end
 
